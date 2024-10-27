1319c98e3a50232fa7b475e20e62f0f21e04d7f0
LeetDimas1337 <142295284+LeetDimas1337@users.noreply.github.com>
2024-10-27 13:21:27 +0300

    захардкодил env

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,4 +1,4 @@
 import axios from 'axios';
 import { appEnv } from '@shared/consts/env';
 
-export const mainApi = axios.create({ baseURL: appEnv.BASE_URL });
+export const mainApi = axios.create({ baseURL: 'https://api.dotg.tech/' });
