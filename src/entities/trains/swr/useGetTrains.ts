dc38f17ae55ad8e54337ab8eb03f01d38873b9e9
LeetDimas1337 <142295284+LeetDimas1337@users.noreply.github.com>
2024-10-27 06:45:34 +0300

    Update useGetTrains.ts

--- src/entities/trains/swr/useGetTrains.ts
+++ src/entities/trains/swr/useGetTrains.ts
@@ -1,14 +1,15 @@
-import useSWRImmutable from 'swr/immutable';
 import { getAllTrains, GetAllTrainsParams } from '@entities/trains/api/wagons.service';
 import dayjs from 'dayjs';
+import useSWR from 'swr';
 
 export const useGetTrains = ({ to, date, from }: GetAllTrainsParams) => {
 	const shouldFetch = [ to, date, from ].every(Boolean);
-	const swrResponse = useSWRImmutable(
+	const swrResponse = useSWR(
 		shouldFetch
 			? [ 'trains', from, to, date ]
-			: null
-		, ([ _key, from, to, date ]) => getAllTrains({ from, date: dayjs(date).add(3,'hours').toISOString(), to })
+			: null,
+		([ _key, from, to, date ]) => getAllTrains({ from, date: dayjs(date).add(3, 'hours').toISOString(), to }),
+		{ refreshInterval: 5000, }
 	);
 	return {
 		...swrResponse,
